92b16bf422a2736fc24dc077119a65ba282694b2
Cano-Simon Luc <luc.cano@hotmail.fr>
2018-12-05 11:37:46 +0100

    add Jquery & bootstrap

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const app = express()
 
+
 app.use(express.static('public'));
+app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist'));
+app.use('/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist/'));
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
