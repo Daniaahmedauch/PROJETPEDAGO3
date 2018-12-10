49ce28b85858644a90904cfdc9f133e91c887a97
nicolas <nicolas.lasportes@gmail.com>
2018-12-10 09:50:31 +0100

    retour au dernier commit de marcé

---
+++ comVisuelle/platforms/browser/www/js/index.js
@@ -0,0 +1,54 @@
+document.addEventListener('deviceready', start, false);
+
+
+var db;
+
+
+
+function start()
+{
+    db = window.openDatabase("data", "1.0", "data test", 200000);
+    db.transaction(populateDB, errorCB, successCB);
+
+    $('#orel').on("click", check);
+}    
+
+function check()
+{
+    console.log('essai acces DB');
+    db.transaction(checkDB, errorCB, successCB);
+}
+
+function checkDB(tx)
+{
+    tx.executeSql('SELECT * FROM categories;', [], function(tx, result){
+        console.log('acces effectue');
+        console.log(result.rows[0]);
+    });
+}
+
+
+
+
+function populateDB(tx)
+{
+    // tx.executeSql('DROP TABLE IF EXISTS categories');
+    tx.executeSql('CREATE TABLE IF NOT EXISTS "categories" ( `category_id` INTEGER PRIMARY KEY AUTOINCREMENT, `category_name` TEXT)');
+    tx.executeSql('INSERT INTO categories (category_id, category_name) VALUES (1, "aliments")');
+    // tx.executeSql('INSERT INTO DEMO (id, data) VALUES (2, "Second row")');
+}
+
+
+function errorCB(tx, err)
+{
+    alert("Error processing SQL: "+err);
+}
+
+
+function successCB()
+{
+    alert("success!");
+}
+
+
+
