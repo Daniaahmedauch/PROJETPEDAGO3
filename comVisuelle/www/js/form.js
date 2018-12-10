1fda57ff13f07232ba71cfd2109c654187730cac
Taskhgar <marc.lupi@protonmail.com>
2018-12-10 09:29:51 +0100

    ajout des dernières modifications apportées au formulaire

--- comVisuelle/www/js/form.js
+++ comVisuelle/www/js/form.js
@@ -1,11 +1,24 @@
-// reset application on startup STANDBY !
+// FORM
+// reset form at startup
 //  function resetForm()
 //     {
-//     document.getElementById('#user_entry').reset();
+//     document.getElementById('#mainForm').reset();
 // }
 //
 // resetForm();
 
+// IMAGE PATH
+// display the thumbnail of the image and the image path
+$('#inputFile').change( function(event)
+    {
+    var imagePath = URL.createObjectURL(event.target.files[0]);
+    $("img").fadeIn("fast").attr('src',URL.createObjectURL(event.target.files[0]));
+    $("#displayImagePath").html("<strong>" + imagePath + "</strong>");
+
+    console.log(imagePath);
+});
+
+// TAGS
 // display and creation of tags in the same input
 $('#selectTag').select2(
     {
@@ -14,6 +27,14 @@ $('#selectTag').select2(
     tokenSeparators: [',', ' ']
 });
 
+// display tags names in the console
+$("#selectTag").on("select2:select", function (e)
+    { 
+    var select_value_tag = $(e.currentTarget).val();
+    console.log(select_value_tag)
+});
+
+// CATEGORIES
 // display and creation of categories in the same input
 $('#selectCategory').select2(
     {
@@ -22,21 +43,7 @@ $('#selectCategory').select2(
     tokenSeparators: [',', ' ']
 });
 
-// display images on the console
-$("#insertImagegit pu").on("select2:select", function (e)
-    { 
-    var select_value_tag = $(e.currentTarget).val();
-    console.log(select_value_tag)
-});
-
-// display tags on the console
-$("#selectTag").on("select2:select", function (e)
-    { 
-    var select_value_tag = $(e.currentTarget).val();
-    console.log(select_value_tag)
-});
-
-// display categories on the console
+// display categories names in the console
 $("#selectCategory").on("select2:select", function (e)
     { 
     var select_value_category = $(e.currentTarget).val();
