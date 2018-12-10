9518e4aaf37e078f1570e2906fc5b6a0812a30ea
nicolas <nicolas.lasportes@gmail.com>
2018-12-10 11:32:14 +0100

    je sais plus ou j'en suis

--- comVisuelle/www/js/index.js
+++ comVisuelle/www/js/index.js
@@ -1,46 +1,73 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-    },
+document.addEventListener('deviceready', start, false);
 
-    // deviceready Event Handler
-    //
-    // Bind any cordova events here. Common events are:
-    // 'pause', 'resume', etc.
-    onDeviceReady: function() {
-        this.receivedEvent('deviceready');
-    },
 
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+var db;
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
 
-        console.log('Received Event: ' + id);
-    }
-};
 
-app.initialize();
+function start()
+{
+    db = window.openDatabase("data", "1.0", "data test", 200000);
+    db.transaction(populateDB, errorCB, successCB);
+
+    $('#orel').on("click", check);
+}    
+
+function check()
+{
+    console.log('essai acces DB');
+    db.transaction(checkDB, errorCB, successCB);
+}
+
+function checkDB(tx)
+{
+    tx.executeSql('SELECT * FROM categories;', [], function(tx, result){
+        console.log('acces effectue');
+        console.log(result.rows);
+    });
+}
+
+
+
+
+function populateDB(tx)
+{
+
+    tx.executeSql('CREATE TABLE IF NOT EXISTS "categories" ( `category_id` INTEGER PRIMARY KEY AUTOINCREMENT, `category_name` TEXT)');
+
+    tx.executeSql('INSERT INTO `categories` VALUES (2,'aliments','','')');
+    tx.executeSql('INSERT INTO `categories` VALUES (3,'actions','','')');
+    tx.executeSql('INSERT INTO `categories` VALUES (4,'activités','','')');
+    tx.executeSql('INSERT INTO `categories` VALUES (5,'hygiène','','')');
+    tx.executeSql('INSERT INTO `categories` VALUES (6,'santé','','')');
+    tx.executeSql('INSERT INTO `categories` VALUES (7,'sorties','','')');
+    tx.executeSql('INSERT INTO `categories` VALUES (8,'loisirs','','')');
+    tx.executeSql('INSERT INTO `categories` VALUES (9,'humeurs','','')');
+    tx.executeSql('INSERT INTO `categories` VALUES (10,'temps','','')');
+    tx.executeSql('INSERT INTO `categories` VALUES (11,'interdit','','')');
+    tx.executeSql('INSERT INTO `categories` VALUES (12,'intéractions','','')');
+    tx.executeSql('INSERT INTO `categories` VALUES (13,'sports','','')');
+    tx.executeSql('INSERT INTO `categories` VALUES (14,'lieux','','')');
+    tx.executeSql('INSERT INTO `categories` VALUES (15,'évènements','','')');
+    tx.executeSql('INSERT INTO `categories` VALUES (16,'objets','','')');
+    tx.executeSql('INSERT INTO `categories` VALUES (17,'habits','','')');
+    tx.executeSql('INSERT INTO `categories` VALUES (18,'gestes','','')');
+    tx.executeSql('INSERT INTO `categories` VALUES (19,'intéractions avec pros','','')');
+    tx.executeSql('INSERT INTO `categories` VALUES (20,'émotions','','')');
+    tx.executeSql('INSERT INTO `categories` VALUES (21,'états','','')');
+}
+
+
+function errorCB(tx, err)
+{
+    alert("Error processing SQL: "+err);
+}
+
+
+function successCB()
+{
+    alert("success!");
+}
+
+
+
