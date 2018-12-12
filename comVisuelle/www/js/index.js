4696acee531dc4d14528772a8aa44b4db05a9b84
Adeline <adeline.contact@gmail.com>
2018-12-12 10:16:48 +0100

    version fonctionnelle

--- comVisuelle/www/js/index.js
+++ comVisuelle/www/js/index.js
@@ -1,4 +1,4 @@
-document.addEventListener('deviceready', start, false);
+// document.addEventListener('deviceready', onDeviceReady, false);
 
 var db;
 
@@ -18,10 +18,17 @@ function check()
 
 function checkDB(tx)
 {
-    tx.executeSql('SELECT * FROM pictures;', [], function(tx, result)
+    tx.executeSql('SELECT * FROM categories;', [], function(tx, result)
     {
         console.log('acces effectue');
         console.log(result.rows);
+        var resultat = result.rows;
+
+        for(var i=0; i<resultat.length; i++){
+            $('.dropdown-menu').html('<a class="dropdown-item" href="#">'+resultat[i]+'</a>');
+        }
+        
+        // return resultat;
     });
 }
 
@@ -187,7 +194,7 @@ function populateDB(tx)
     tx.executeSql("INSERT INTO pictures (picture_id,picture_name,picture_url) VALUES (153,'yaourts','images/yaourts.JPG')");
     tx.executeSql("INSERT INTO pictures (picture_id,picture_name,picture_url) VALUES (154,'zumba','images/zumba.JPG')");
     tx.executeSql("INSERT INTO pictures (picture_id,picture_name,picture_url) VALUES (155,'judo','images/judo.JPG')");
-       
+
     tx.executeSql("INSERT INTO categories (category_id, category_name) VALUES (2,'aliments')");
     tx.executeSql("INSERT INTO categories (category_id, category_name) VALUES (3,'actions')");
     tx.executeSql("INSERT INTO categories (category_id, category_name) VALUES (4,'activités')");
@@ -219,3 +226,67 @@ function successCB()
 {
     alert("success!");
 }
+
+/**
+ * Fonction événement, click pour intégrer une page html (form_html.html) à la page html par défaut index.html
+ */
+
+$('#admin').on("click", function(){
+
+    // $('#display').append('<div id="mainForm"><form method="POST" action=""><div><legend id="legende">FORMULAIRE D\'INSERTION</legend></div><div class="form-group"><label for="image">1°) SÉLECTIONNER UNE IMAGE</label><input id="inputFile" type="file" accept=".jpg, .jpeg, .png, .svg" class="form-control-file" aria-describedby="fileHelp"></div><img src="" width="200" style="display:none;"/><div id="displayImagePath"></div><hr><div class="form-group"><label for="tag">2°) SÉLECTIONNER ET/OU CRÉER UN/PLUSIEURS TAGS</label><!-- formulaire select2 --><div><select id="selectTag" class="form-control" multiple="multiple"><option selected="selected" value="orange">Orange</option><option selected="selected" value="white">White</option><option selected="selected" value="brown">Brown</option><option selected="selected" value="purple">Purple</option><option selected="selected" value="grey">Grey</option><option selected="selected" value="black">Black</option><option selected="selected" value="yellow">Yellow</option></select></div><!-- <select class="form-control" id="insertTage"><option value="_">_</option><option value="tag 1">Tag 1</option><option value="tag 2">Tag 2</option><option value="tag 3">Tag 3</option><option value="tag 4">Tag 4</option><option value="tag 5">Tag 5</option></select><button class="btn btn-success" type="button">Insérer</button><input type="text" class="form-control" placeholder="insérer un tag" id="insertTagButton"><button class="btn btn-success" type="button">Insérer</button><textarea class="form-control" id="user_entry" rows="1" placeholder="Vos tags s\'affichent ici"></textarea> --></div><hr><div class="form-group"><label for="categorie">3°) SÉLECTIONNER ET/OU CRÉER UNE CATÉGORIE</label><!-- formulaire select2 --><div><select id="selectCategory" class="form-control" multiple="multiple"><option selected="selected" value="orange">Orange</option><option selected="selected" value="white">White</option><option selected="selected" value="brown">Brown</option><option selected="selected" value="purple">Purple</option><option selected="selected" value="grey">Grey</option><option selected="selected" value="black">Black</option><option selected="selected" value="yellow">Yellow</option></select></div><!-- <select class="form-control" id="insertCategory"><option value="_">_</option><option value="hygiene">Hygiène</option><option value="sante">Santé</option><option value="sport">Sport</option><option value="aliments">Aliments</option><option value="jeux">Jeux</option></select><button class="btn btn-success" type="button">Insérer</button><input type="text" class="form-control" placeholder="Insérer une catégorie" id="insertCategoryButton"><button class="btn btn-success" type="button">Insérer</button><textarea class="form-control" id="user_entry" rows="1" placeholder="Votre catégorie s\'affiche ici"></textarea> --></div><div><button id="confirmButton" type="submit" class="btn btn-success">Enregistrer</button></div></form></div>');
+    // location.href="../form_html.html";
+
+    /* 
+    * vide la div qui accueille le iframe, avant de le remplir à nouveau, ceci pour éviter des iframes multiples 
+    */
+    $('#display').html("");
+
+    /* 
+    * Intégration d'un iframe dans la div #display du index.html 
+    */
+
+    // utiliser cette ligne pour le "cordova run android"
+    $('#display').append('<iframe width="100%" height="600" sandbox="allow-scripts allow-forms allow-same-origin allow-modals" seamless src="file:///android_asset/www/form_html.html">Le navigateur n\'est pas compatible></iframe>');
+    
+    // utiliser cette ligne pour le "cordova run browser"
+    //$('#display').append('<iframe width="100%" height="600" sandbox="allow-scripts" seamless src="../form_html.html">Le navigateur n\'est pas compatible></iframe>');
+
+});
+
+
+/**
+ * Récupérer une image du gestionnaire de fichier de la tablette
+ * Nécessite l'installation du plugin cordova camera : 
+ * Dans le terminal :
+ * $ cordova plugin add cordova-plugin-camera 
+ */
+
+function cameraGetPicture() {
+    navigator.camera.getPicture(onSuccess, onFail, { quality: 50,
+        destinationType: Camera.DestinationType.FILE_URI,
+        sourceType: Camera.PictureSourceType.SAVEDPHOTOALBUM,
+        targetWidth: 100,
+        targetHeight: 100
+    });
+    
+    function onSuccess(imageURL) {
+        var image = document.getElementById('myImage');
+        image.src = imageURL;
+    }
+    
+    function onFail(message) {
+        alert('Failed because: ' + message);
+    }
+    
+}
+/**
+ * Fonction événenement, au click, ouverture du gestionnaire de fichiers local
+ */
+
+function onDeviceReady(){
+    // window.requestFileSystem(LocalFileSystem.PERSISTENT, 1024*1024, successCB, errorHandler);
+    console.log('c\'est ok');
+    $("#inputFile").on("click", cameraGetPicture);
+}
+
+document.addEventListener('deviceready', onDeviceReady, false);
